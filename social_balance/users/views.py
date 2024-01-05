b14a4465fdfc8b445c1e873846c01b82c0de50f9
carlosharo-admin <carlos.haro@jakaysa.com>
2024-01-05 15:20:34 -0500

    Include idusuario in the response of CustomTokenObtainPairView.

--- social_balance/users/views.py
+++ social_balance/users/views.py
@@ -908,6 +908,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
                 user = Jusuarios.objects.get(email=request.data["email"])
                 user.last_login = timezone.now()
                 user.save()
+                response.data["idusuario"] = user.idusuario
                 response.data["name"] = user.first_name + user.last_name
                 response.data["idrol"] = user.idrol.idrol if user.idrol else None
                 response.data["role"] = user.idrol.nombrerol if user.idrol else None
