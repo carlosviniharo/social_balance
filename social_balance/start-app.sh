22eef4f8c10a561b660513dc5872ce688805af6f
carlosharo-admin <carlos.haro@jakaysa.com>
2024-01-11 15:27:37 -0500

    Improved the initializer so now includes a bash script.

---
+++ social_balance/start-app.sh
@@ -0,0 +1,7 @@
+# Wait for the Database to be ready
+while ! nc -z db 5432; do sleep 1; done
+# Initialize Django
+python manage.py makemigrations
+python manage.py migrate
+# Start Django
+python manage.py runserver 0.0.0.0:8000
