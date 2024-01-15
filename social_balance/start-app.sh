28372df12c2a82d7e2ca3db17e05a24f9a2b50f6
carlosharo-admin <carlos.haro@jakaysa.com>
2024-01-15 16:58:38 -0500

    Task BS-98. Included values (ony the three first indicators of the principle one) for the entry variablesgrafico in the table jindicadores.

--- social_balance/start-app.sh
+++ social_balance/start-app.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
 # Wait for the Database to be ready
-while ! nc -z db 5432; do sleep 1; done
+while ! nc -z db 5432; do
+    sleep 1
+done
+
 # Initialize Django
 python manage.py makemigrations
 python manage.py migrate
+
 # Start Django
 python manage.py runserver 0.0.0.0:8000
