4994325fba96e0e41aa40eda103b52be94bbed92
carlosharo-admin <carlos.haro@jakaysa.com>
2024-02-08 16:07:10 -0500

    Task: BS-216. Corrected the position of the call of the variablesunidades array in vindicators and vobjectivesvalues. Included populate_table function in the helper of the report's app. Included in the template the entries for populating the section 5.

--- social_balance/reports/utils/helper.py
+++ social_balance/reports/utils/helper.py
@@ -8,6 +8,8 @@ from docxtpl import DocxTemplate, InlineImage
 import requests
 from rest_framework.exceptions import APIException
 
+from principles.models import Vindicators
+
 current_directory = os.getcwd()
 url = os.path.join(current_directory, "reports", "templates", "docx", "socialBalanceTemplate.docx")
 
@@ -32,8 +34,11 @@ def create_report(principles_dict_list, objects_reports_dic_list):
     with ThreadPoolExecutor() as executor:
         executor.map(process_objects, objects_reports_dic_list)
 
+    summary_dic = populate_table(objects_reports_dic_list)
+
     context = {"Author": "Carlos",
-               "data": report_docx_dic
+               "data_indicators": report_docx_dic,
+               "data_summary": summary_dic
                }
 
     doc_social_balance.render(context)
@@ -51,7 +56,6 @@ def create_report(principles_dict_list, objects_reports_dic_list):
 
     # Close the buffer
     buffer.close()
-
     return response
 
 
@@ -73,3 +77,72 @@ def retrieve_image_aws(template, dict_indicators):
     dict_indicators["graficocontenido"] = imagen
 
     dict_indicators["table"] = False
+
+
+def populate_table(objects_reports_dic_list):
+
+    indicators_overview = {
+        "sum_values": {
+            "sum_all_indicators": 0,
+            "sum_indicators_not_applying": 0,
+            "sum_indicators_accomplished": 0,
+            "sum_indicators_failed": 0,
+            "sum_accomplishment": 0.0
+        }
+    }
+
+    all_indicators = Vindicators.objects.all()
+
+    # Sum of all the indicators
+    sum_all_indicators = len(all_indicators)
+    indicators_overview["sum_values"]["sum_all_indicators"] = sum_all_indicators
+
+    for indicators in all_indicators:
+        indicators_overview.setdefault(
+            indicators.codigoprincipio,
+            {
+                "total_indicators": 0,
+                "indicators_not_applying": 0,
+                "indicators_accomplished": 0,
+                "indicators_failed": 0,
+                "accomplishment_percentage": 0,
+             }
+        )
+        indicators_overview[indicators.codigoprincipio]["total_indicators"] += 1
+
+    for indicators_values in objects_reports_dic_list:
+        codeprinciple = indicators_values.get("codigoprincipio")
+        accomplishment = indicators_values.get("cumplimiento")
+
+        if not codeprinciple:
+            raise APIException("The report is empty, it does not have any record")
+
+        if accomplishment is None:
+            indicators_overview[codeprinciple]["indicators_not_applying"] += 1
+            indicators_overview["sum_values"]["sum_indicators_not_applying"] += 1
+
+        elif accomplishment is True:
+            indicators_overview[codeprinciple]["indicators_accomplished"] += 1
+            indicators_overview["sum_values"]["sum_indicators_accomplished"] += 1
+
+        elif accomplishment is False:
+            indicators_overview[codeprinciple]["indicators_failed"] += 1
+            indicators_overview["sum_values"]["sum_indicators_failed"] += 1
+
+        valid_indicators = (
+                indicators_overview[codeprinciple]["total_indicators"] -
+                indicators_overview[codeprinciple]["indicators_not_applying"]
+        )
+        percentage_accomplishment = (
+            indicators_overview[codeprinciple]["indicators_accomplished"] /
+            valid_indicators
+                                    ) * 100
+        indicators_overview[codeprinciple]["accomplishment_percentage"] = round(percentage_accomplishment, 2)
+
+    all_valid_indicators = sum_all_indicators - indicators_overview["sum_values"]["sum_indicators_not_applying"]
+    indicators_overview["sum_values"]["sum_accomplishment"] = round(
+        (indicators_overview["sum_values"]["sum_indicators_accomplished"] / all_valid_indicators) * 100,
+        2
+    )
+
+    return indicators_overview
