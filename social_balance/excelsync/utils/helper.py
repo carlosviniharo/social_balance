b4c9b3f5d2bb230aeb7193d64de3800c6b17fc3f
carlosharo-admin <carlos.haro@jakaysa.com>
2024-02-22 11:11:45 -0500

    Task: BS-250. Wrote the first line for digesting the Excel information.

--- social_balance/excelsync/utils/helper.py
+++ social_balance/excelsync/utils/helper.py
@@ -1,10 +1,18 @@
 from openpyxl import load_workbook
 import pandas as pd
 
+from principles.models import Vindicators
+
 
 def extract_excel_to_json(excel_file):
     # Open the excel file.
     workbook_two = load_workbook(excel_file)
-    checklistsheet = workbook_two["Check list"]
-    df = pd.DataFrame(checklistsheet.values)
+    df = pd.DataFrame()
+    for principle_sheet in workbook_two:
+        df = pd.DataFrame(principle_sheet.values)
+        indicators = Vindicators.objects.all()
+        # indicators.get(codigoindicador=df.tolist())
+        checklistsheet = principle_sheet
+        df = pd.DataFrame(checklistsheet.values)
+
     return df.to_json()
