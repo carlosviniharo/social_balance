a02dfcf109bf2263485cb0bb912f87983f8c08fb
carlosharo-admin <carlos.haro@jakaysa.com>
2024-03-19 15:49:59 -0500

    Bug: Fixed the bash file so ti does not have conflicts with the main branch.

--- social_balance/start-app.sh
+++ social_balance/start-app.sh
@@ -1,13 +1,28 @@
-## Wait for the Database to be ready
-#while ! nc -z db 5432; do
-#    sleep 1
-#done
+#!/bin/bash
+
+# Function to wait for PostgreSQL to be ready
+wait_for_postgresql() {
+    while ! nc -z db 5432; do
+        echo "Waiting for PostgreSQL to be ready..."
+        sleep 2
+    done
+}
+
+# Function to initialize Django
+initialize_django() {
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py collectstatic --noinput
+}
+
+# Wait for the Database to be ready
+#wait_for_postgresql
 
 # Initialize Django
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput
+initialize_django
 
 # Start Django
-apache2ctl -D FOREGROUND
-#python manage.py runserver 0.0.0.0:8000
+# Uncomment the appropriate line based on your preference
+ apache2ctl -D FOREGROUND
+# OR
+#python manage.py runserver 0.0.0.0:8001
